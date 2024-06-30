10bff935d34c7ced6d26bb31e19b5ae67b60c34e
markusz <markus_zhang@hotmail.com>
2024-06-30 08:19:59 -0400

    More practices

--- TextReader/TextReader.c
+++ TextReader/TextReader.c
@@ -11,16 +11,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 	MSG				msg;
 	WNDCLASS		wndclass;
 
-	wndclass.style			= CS_HREDRAW | CS_VREDRAW;
-	wndclass.lpfnWndProc	= WndProc;
-	wndclass.cbClsExtra		= 0;
-	wndclass.cbWndExtra		= 0;
-	wndclass.hInstance		= hInstance;
-	wndclass.hIcon			= LoadIcon(NULL, IDI_APPLICATION);
-	wndclass.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wndclass.hbrBackground	= (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wndclass.lpszMenuName	= NULL;
-	wndclass.lpszClassName	= szAppName;
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = szAppName;
 
 	if (!(RegisterClass(&wndclass))) {
 		MessageBox(NULL, TEXT("This program requires Windows NT!"), szAppName, MB_ICONERROR);
